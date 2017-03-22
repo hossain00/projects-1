ba548f1315497ca291de15f740224cb2eb43ff87
Kam Lam <kamlam@live.com>
2017-03-22 16:30:49 -0400

    Up to date

--- sas/src/main/java/com/getterandsetter/hibernate/ApplicantDAOImpl.java
+++ sas/src/main/java/com/getterandsetter/hibernate/ApplicantDAOImpl.java
@@ -1,10 +1,14 @@
 package com.getterandsetter.hibernate;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 import com.getterandsetter.hibernate.HibernateUtils;
+
+
 
 public class ApplicantDAOImpl implements ApplicantDAO {
 
@@ -36,7 +40,7 @@ public class ApplicantDAOImpl implements ApplicantDAO {
 			tx.rollback();
 		}
 	}
-	
+	//only using this to create or add application
 	public void createSas_Application(Sas_Application app) {
 		Transaction tx = session.beginTransaction();
 		try{
@@ -48,4 +52,36 @@ public class ApplicantDAOImpl implements ApplicantDAO {
 			tx.rollback();
 		}
 	}
+	
+	//use this to update application
+	public void update(Sas_Application app) {
+		Transaction tx = session.beginTransaction();
+		session.saveOrUpdate(app);
+		tx.commit();
+	}
+	
+	//delete application which we won't do
+	public void delete(Sas_Application app) {
+		Transaction tx = session.beginTransaction();
+		session.delete(app);
+		tx.commit();
+	}
+	
+	//this finds the application by its ID
+	public Sas_Application findOne(int appid) {
+		Sas_Application application = (Sas_Application) session.load(Sas_Application.class, appid); //generate SELECT
+		return application;
+	}
+	
+	//get all the applications in a list
+
+    @SuppressWarnings("unchecked")
+
+    public List<Sas_Application> findAllApps() {
+
+        //return session.createQuery("from Sas_Application").list();
+
+        return session.createCriteria(Sas_Application.class).list();
+
+    }
 }
