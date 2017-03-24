8e779cea59764335c9dcd8b9db5616a1281cd9d9
Kam Lam <kamlam@live.com>
2017-03-23 21:22:36 -0400

    Refactor Part two CALL ME MIChAEL CALL ME MICHAEL

--- sas/src/main/java/com/getterandsetter/hibernate/CreateDAO.java
+++
@@ -1,7 +0,0 @@
-package com.getterandsetter.hibernate;
-
-public interface CreateDAO 
-{
-	public void createStatus(Sas_Application_Status status);
-	public void createAppType(Sas_Application_Type type);
-}
