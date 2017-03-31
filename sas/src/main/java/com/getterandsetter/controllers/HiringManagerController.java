6818aa9a6b8927f330a7e4b349167f4dedb64805
Kam Lam <kamlam@live.com>
2017-03-31 00:40:15 -0400

    HIreManagerCOntroller Need to see how to Post job to data Base

--- sas/src/main/java/com/getterandsetter/controllers/HiringManagerController.java
+++ sas/src/main/java/com/getterandsetter/controllers/HiringManagerController.java
@@ -18,15 +18,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.getterandsetter.beans.Sas_Application;
 import com.getterandsetter.beans.Sas_Application_Status;
+import com.getterandsetter.beans.Sas_Application_Type;
 import com.getterandsetter.beans.Sas_Users;
 import com.getterandsetter.dao.HiringManagerDAOImpl;
 import com.getterandsetter.obsolete.ApplicantDAOImpl;
 
+
 @Controller
 @RequestMapping(value = "hmanager")
 public class HiringManagerController {
+	
 	private HiringManagerDAOImpl d = new HiringManagerDAOImpl();
 	private Sas_Application a = new Sas_Application();
+	private List<Sas_Application_Type> apptype = new ArrayList<>();
 	private List<Sas_Application> app = new ArrayList<>();
 	// jobs = new ApplicantDAOImpl().findAllJobs();
 
@@ -36,6 +40,20 @@ public class HiringManagerController {
 
 	}
 	
+	@RequestMapping(value="createpost", method=RequestMethod.POST, 
+			consumes=MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody //write directly to response body.. no redirection
+	
+	public ResponseEntity<String> add(@Valid @RequestBody Sas_Application_Type apptype){
+		System.out.println("Add Post: " + apptype);
+		// d.postJob(d.findManager(1), desiredSkills, job);                              
+		return new ResponseEntity<String>("Success", HttpStatus.CREATED);
+	}
+	
+	
+	
+	
+	
 	@RequestMapping(value = "recom", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
 	public ResponseEntity<List<Sas_Application>> Reommended(){
@@ -43,7 +61,7 @@ public class HiringManagerController {
 		return new ResponseEntity<List<Sas_Application>>(app, HttpStatus.OK);
 	}
 
-	@RequestMapping(value = "appdate", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "appupdate", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
 	public ResponseEntity<String> add(@Valid @RequestBody Sas_Application app) {
 		// System.out.println("Client msg: " + req.getParameter("hello"));
